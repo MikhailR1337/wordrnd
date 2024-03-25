1de1e346ac8f18e8aeed640443a7a46a60111637
Mikhail Rogov <fanizor1337@gmail.com>
2024-03-25 21:33:50 +0800

    finished mvp#2

---
+++ components/TabBar.tsx/index.tsx
@@ -0,0 +1,32 @@
+import { View, Text, StyleSheet, Pressable } from "react-native"
+import { BottomTabBarProps } from '@react-navigation/bottom-tabs'
+import { useNavigation } from '@react-navigation/native'
+
+
+export const TabBar = ({ state }: BottomTabBarProps) => {
+    const navigation = useNavigation()
+
+    return (
+        <View style={styles.container}>
+            <Pressable style={styles.pressable} onPress={() => navigation.navigate(state.routes[0].name)}>
+                <Text>Vocabulary</Text>
+            </Pressable>
+            <Pressable style={styles.pressable} onPress={() => navigation.navigate(state.routes[1].name)}>
+                <Text>Studying</Text>
+            </Pressable>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+      paddingBottom: 16,
+      flexDirection: 'row',
+      backgroundColor: '#fff',
+      alignItems: 'flex-end',
+      justifyContent: 'center',
+      gap: 20,
+    },
+    pressable: {
+        borderWidth: 1, borderColor: '#000000', borderRadius: 8, paddingVertical: 10, paddingHorizontal: 16
+    }
+  });
